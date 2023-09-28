896c02c539c7786ad07c68584fcd4436db5cd15c
MADUSHANI A.G.D <madushaniagd@gmail.com>
2023-09-28 19:58:35 +0530

    End Node module folder

--- first-app/app.js
+++ first-app/app.js
@@ -1,58 +1,4 @@
-function sayHello(name) {
-  console.log('Hello ' + name);
-}
-
-// sayHello('Dimalsha');
-//console.log(window); //window is a global object in browser
-
-//global objects
-// console.log(); //global object
-// setTimeout(); //global object
-// clearTimeout(); //global object
-// setInterval(); 
-// clearInterval();
-
-//window.console.log();  //console is a property of window object
-
-let message = '';
-global.console.log(global.message); //global object, since we are in node, window object is not available
-
-
-console.log(module); //module is a global object in node
-
-//loading a module
-const log = require('./logger'); //logger is a local module, we use ./ to indicate that it is in the same folder
-console.log(log);
-log('message'); //calling the log function in logger.js
-
-//path module
-const path = require('path');
-
-let pathObj = path.parse(__filename);
-console.log(pathObj);
-
-//os module
-const os = require('os');
-let totalMemory = os.totalmem();
-let freeMemory = os.freemem();
-console.log('Total Memory: ' + totalMemory);
-//Template string
-//ES6 / ES2015 : ECMAScript 6
-console.log(`Free Memory: ${freeMemory}`);
-
-//file system module
-const fs = require('fs');
-const files = fs.readdirSync('./');
-console.log(files);
-
-//asynchronous version //recommended
-fs.readdir('./', function(err, files) {
-  if (err) console.log('Error', err);
-  else console.log('Result', files);
-});
-
-//events module
-const EventEmitter = require('events'); //EventEmitter is a class //blueprint of an object
+EventEmitter = require('events'); //EventEmitter is a class //blueprint of an object
 const emitter = new EventEmitter(); //emitter is an object, instance of EventEmitter class
 
 //register a listener
@@ -61,4 +7,27 @@ emitter.on('messageLogged', (arg) => {
 });
 
 const Logger = require('./logger');
-log('message');
+Logger('message');
+
+//http module
+const http = require('http');
+// we don't use the http module directly, we use the express framework
+//because this module is low level, we have to do a lot of work to build a web server
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello World');
+    res.end();
+  }
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+}); //server is an EventEmitter
+
+//register a listener
+server.on('connection', (socket) => {
+  console.log('New connection...');
+});
+
+server.listen(3000);
+console.log('Listening on port 3000...');
