91e5e35f08e336c3894dec4f6f891acb544440e9
MADUSHANI A.G.D <madushaniagd@gmail.com>
2023-09-29 13:03:44 +0530

    handling http get requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,18 +3,29 @@ const express = require("express");
 //invoke const express and store the resulting application in const app
 const app = express();
 
+const courses = [  
+    { id: 1, name: "course1" },
+    { id: 2, name: "course2" },
+    { id: 3, name: "course3" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!!!"); //send a response to the client
 });
 
 //add a new route to return a list of courses
 app.get("/api/courses", (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
  });
 
-app.get("/api/posts/:year/:month", (req, res) => {
-    res.send(req.params);
+ //add a new route to return a list of 
+app.get("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //if not existing, return 404 - object not found
+    if(!course) res.status(404).send("The course with the given ID was not found."); //404 object not found
+    res.send(course);//return the object
 });
+
 
  //environment variable
 const port = process.env.PORT || 3000;
